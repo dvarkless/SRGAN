0074d28be48c03b3d7fe655ef2336830da3a7230
dvarkless <amir911157@yandex.ru>
2022-10-28 15:01:56 +0300

    Modify model.py: hardcode upscale=2, change var names

--- model.py
+++ model.py
@@ -1,16 +1,13 @@
-import math
 import torch
 from torch import nn
 
 
 class Generator(nn.Module):
-    def __init__(self, scale_factor):
-        upsample_block_num = int(math.log(scale_factor, 2))
-
-        super(Generator, self).__init__()
+    def __init__(self):
+        super().__init__()
         self.block1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=9, padding=4),
-            nn.PReLU()
+            nn.PReLU(),
         )
         self.block2 = ResidualBlock(64)
         self.block3 = ResidualBlock(64)
@@ -21,26 +18,24 @@ class Generator(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64)
         )
-        block8 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
-        block8.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
-        self.block8 = nn.Sequential(*block8)
+        self.block8 = UpsampleBlock(64)
 
     def forward(self, x):
-        block1 = self.block1(x)
-        block2 = self.block2(block1)
-        block3 = self.block3(block2)
-        block4 = self.block4(block3)
-        block5 = self.block5(block4)
-        block6 = self.block6(block5)
-        block7 = self.block7(block6)
-        block8 = self.block8(block1 + block7)
+        x_initial = self.block1(x)
+        x = self.block2(x_initial)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.block7(x)
+        x = self.block8(x_initial + x)
 
-        return (torch.tanh(block8) + 1) / 2
+        return torch.sigmoid(x)
 
 
 class Discriminator(nn.Module):
     def __init__(self):
-        super(Discriminator, self).__init__()
+        super().__init__()
         self.net = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.LeakyReLU(0.2),
@@ -86,7 +81,7 @@ class Discriminator(nn.Module):
 
 class ResidualBlock(nn.Module):
     def __init__(self, channels):
-        super(ResidualBlock, self).__init__()
+        super().__init__()
         self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(channels)
         self.prelu = nn.PReLU()
@@ -103,15 +98,17 @@ class ResidualBlock(nn.Module):
         return x + residual
 
 
-class UpsampleBLock(nn.Module):
-    def __init__(self, in_channels, up_scale):
-        super(UpsampleBLock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1)
-        self.pixel_shuffle = nn.PixelShuffle(up_scale)
+class UpsampleBlock(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, in_channels * 4, kernel_size=3, padding=1)
+        self.pixel_shuffle = nn.PixelShuffle(2)
         self.prelu = nn.PReLU()
+        self.reducer = nn.Conv2d(64, 3, kernel_size=9, padding=4)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.pixel_shuffle(x)
         x = self.prelu(x)
+        x = self.reducer(x)
         return x
