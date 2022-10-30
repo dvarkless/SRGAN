607aafd0a2a926fc0d4e7ce757f7f32a2929ee2f
dvarkless <amir911157@yandex.ru>
2022-10-30 20:15:03 +0300

    Implement saving singular losses in the current batch

--- loss.py
+++ loss.py
@@ -7,11 +7,12 @@ class GeneratorLoss(nn.Module):
     def __init__(self):
         super().__init__()
         loss_network = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2).eval()
-        print(loss_network.__class__.__name__)
         for param in loss_network.parameters():
             param.requires_grad = False
         self.loss_network = loss_network
         self.mse_loss = nn.MSELoss()
+        self.clear_losses()
+
 
     def forward(self, out_labels, out_images, target_images):
         # Adversarial Loss
@@ -20,9 +21,19 @@ class GeneratorLoss(nn.Module):
         perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
         # Image Loss
         image_loss = self.mse_loss(out_images, target_images)
+        self.image_loss = self.image_loss + image_loss
+        self.adv_loss = self.adv_loss + 0.001 * adversarial_loss
+        self.perc_loss = self.perc_loss + 0.007 * perception_loss
 
         return image_loss + 0.001 * adversarial_loss + 0.007 * perception_loss
 
+    def get_losses(self):
+        return self.image_loss, self.adv_loss, self.perc_loss
+
+    def clear_losses(self):
+        self.image_loss = 0.0
+        self.adv_loss = 0.0
+        self.perc_loss = 0.0
 
 if __name__ == "__main__":
     g_loss = GeneratorLoss()
