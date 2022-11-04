56778a198f05adadc8ae877c2c5ce70f036e123a
dvarkless <amir911157@yandex.ru>
2022-11-04 20:13:24 +0300

    Implement training for mse or gan only models + choose image augmentations for training dataset

--- train.py
+++ train.py
@@ -17,19 +17,23 @@ from torch.utils.data import DataLoader
 import pytorch_ssim
 from data_utils import (TrainDatasetFromFolder, ValDatasetFromFolder,
                         display_transform, get_logging_handler)
-from loss import AdversarialLoss, FullLoss
+from loss import AdversarialLoss, FullLoss, MSELoss
 from model import Discriminator, Generator
 
 shutup.please()
 
 
 class Trainer:
-    __modes = ['mse', 'gan', 'full']
+    __model_types = ['mse', 'gan', 'full']
+    __loss_types = {'mse': MSELoss,
+                    'gan': AdversarialLoss,
+                    'full': FullLoss}
+
     def __init__(self, crop_size=120, epochs=100,
                  gen_optimizer=None, disc_optimizer=None,
                  gen_optimizer_params=None, disc_optimizer_params=None,
                  verbose_logs=False, gen_model_name=None,
-                 disc_model_name=None) -> None:
+                 disc_model_name=None, model_type='full') -> None:
 
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG if verbose_logs else logging.INFO)
@@ -42,6 +46,10 @@ class Trainer:
                     (torch.cuda.is_available={self.cuda})'
             self.logger.error(msg)
             raise OSError(msg)
+
+        self.model_type = model_type
+        assert self.model_type in self.__model_types
+        self.has_disc = True if self.model_type != 'mse' else False
 
         if gen_model_name and disc_model_name:
             self.logger.info(
@@ -64,13 +72,20 @@ class Trainer:
         self.logger.info(
             f'G_optimizer is "{gen_optimizer.__name__}", \
               params = {gen_optimizer_params}')
-        self.logger.info(
-            f'D_optimizer is "{disc_optimizer.__name__}", \
-              params = {disc_optimizer_params}')
+        if self.has_disc:
+            self.logger.info(
+                f'D_optimizer is "{disc_optimizer.__name__}", \
+                  params = {disc_optimizer_params}')
         self.gen_optimizer = gen_optimizer(**gen_optimizer_params)
         self.disc_optimizer = disc_optimizer(**disc_optimizer_params)
 
-        self.gen_criterion = GeneratorLoss()
+        self.gen_criterion = self.__loss_types[self.model_type]()
+        if self.model_type == 'mse':
+            self._step_batch = self._step_batch_mse
+        if self.model_type == 'gan':
+            self._step_batch = self._step_batch_gan
+        if self.model_type == 'full':
+            self._step_batch = self._step_batch_full
 
         self.epochs = epochs
         self.crop_size = crop_size
@@ -91,11 +106,17 @@ class Trainer:
         self.gen_criterion.cuda()
         self.logger.debug('CUDA ok')
 
-    def fit(self, train_hr_dir, eval_hr_dir, batch_size=32):
+        if not self.has_disc:
+            del self.disc_model
+            del self.disc_optimizer
+
+    def fit(self, train_hr_dir, eval_hr_dir, batch_size=32,
+            data_augmentation_type='plain'):
         train_hr_dir = Path(train_hr_dir)
         eval_hr_dir = Path(eval_hr_dir)
         train_set = TrainDatasetFromFolder(
-            train_hr_dir, crop_size=self.crop_size)
+            train_hr_dir, crop_size=self.crop_size,
+            transform=data_augmentation_type)
         eval_set = ValDatasetFromFolder(eval_hr_dir)
 
         train_loader = DataLoader(
@@ -103,25 +124,36 @@ class Trainer:
         eval_loader = DataLoader(
             dataset=eval_set, num_workers=4, batch_size=1, shuffle=False)
 
-        bar = alive_it(range(self.epochs), self.epochs)
+        bar = alive_it(range(self.epochs), self.epochs,
+                       calibrate=0.5, force_tty=True,
+                       dual_line=True)
         for epoch in bar:
+            bar.title('SRGAN.fit()')
             if epoch % 10 == 0 and epoch > 0:
-                self._save_models()
+                bar.text('-> Saving models and history...')
+                self._save_models(epoch)
 
+            bar.text(f'-> Training epoch {epoch+1}/{self.epochs}')
             gen_loss, disc_loss, gen_score, disc_score = self._train_epoch(
                 train_loader)
+
             print(f'============= {epoch+1}/{self.epochs} =============')
-            print(f'Training losses: Generator: {gen_loss:.4f}, \
-                  Discriminator: {disc_loss:.4f}')
-            print(f'Generator score: ({gen_score:.3f}), \
-                  Discriminator score: ({disc_score:.3f})')
-            print('Singular losses:')
-            img_l, adv_l, perc_l = self.gen_criterion.get_losses()
             print(
-                f' Image loss = {img_l:.3f}; Adversarial loss = {adv_l:.3f}; \
-                   Perception loss = {perc_l:.3f}')
+                f'Training losses: Generator: {gen_loss:.4f}, Discriminator: {disc_loss:.4f}')
+            print(f'Generator score:     ({gen_score:.3f})')
+            print(f'Discriminator score: ({disc_score:.3f})')
+            print('Separate losses:')
+
+            img_l, adv_l, perc_l = self.gen_criterion.get_losses()
+
+            print(f'Image loss = {img_l:.3f};', end=' ')
+            print(f'Adversarial loss = {adv_l:.3f};', end=' ')
+            print(f'Perception loss = {perc_l:.3f}')
+            bar.text(f'-> Evaluating epoch {epoch+1}/{self.epochs}')
+
             results = self._eval_epoch(eval_loader, epoch)
-            print(f'Evaluating model:')
+
+            print(f'Evaluation results:')
             for name, val in results.items():
                 print(f'{name} = {val:.4f}')
 
@@ -134,22 +166,101 @@ class Trainer:
 
         self._save_models()
 
-    def _save_models(self, epoch=0):
+    def _save_models(self, epoch: int = 0):
         msg = 'saving models and history...'
         self.logger.info(msg)
-        print(msg)
+
         models_path = Path('models')
         if not models_path.exists():
             models_path.mkdir()
 
-        g_name = f'Generator_{date.today().isoformat()}_epoch_{epoch}'
-        d_name = f'Discriminator_{date.today().isoformat()}_epoch_{epoch}'
+        g_name = f'Generator_{date.today().isoformat()}'\
+            + f'_epoch_{epoch}'*bool(epoch)
+        d_name = f'Discriminator_{date.today().isoformat()}'\
+            + f'_epoch_{epoch}'*bool(epoch)
 
         torch.save(self.gen_model.state_dict(), models_path / f'{g_name}.pt')
-        torch.save(self.disc_model.state_dict(), models_path / f'{d_name}.pt')
+        if self.has_disc:
+            torch.save(self.disc_model.state_dict(),
+                       models_path / f'{d_name}.pt')
 
         metric_table_name = f'Training_metrics_{date.today().isoformat()}'
         self.save_metric_data(metric_table_name, epoch)
+
+    def _step_batch_mse(self, lr_img, hr_img):
+        self.gen_model.zero_grad()
+        sr_img = self.gen_model(lr_img)
+        self.logger.debug(f'sr_img info: shape = {sr_img.shape}')
+        gen_loss = self.gen_criterion(sr_img, hr_img)
+        gen_loss.backward()
+        self.gen_optimizer.step()
+
+        return (
+            gen_loss.item(),
+            0,
+            1,
+            0,
+        )
+
+    def _step_batch_gan(self, lr_img, hr_img):
+        sr_img = self.gen_model(lr_img)
+        self.logger.debug(f'sr_img info: shape = {sr_img.shape}')
+
+        self.disc_model.zero_grad()
+
+        real_out = self.disc_model(hr_img).mean()
+        fake_out = self.disc_model(sr_img).mean()
+        self.logger.debug(f'real_out = {real_out}')
+        self.logger.debug(f'fake_out = {fake_out}')
+        disc_loss = -torch.log(1-fake_out) - torch.log(real_out)
+        self.logger.debug(f'disc_loss = {disc_loss}')
+        disc_loss.backward()
+        self.disc_optimizer.step()
+
+        self.gen_model.zero_grad()
+        sr_img = self.gen_model(lr_img)
+        fake_out = self.disc_model(sr_img).mean()
+
+        gen_loss = self.gen_criterion(fake_out, sr_img, hr_img)
+        gen_loss.backward()
+        self.gen_optimizer.step()
+
+        return (
+            gen_loss.item(),
+            disc_loss.item(),
+            fake_out.item(),
+            real_out.item(),
+        )
+
+    def _step_batch_full(self, lr_img, hr_img):
+        sr_img = self.gen_model(lr_img)
+        self.logger.debug(f'sr_img info: shape = {sr_img.shape}')
+
+        self.disc_model.zero_grad()
+
+        real_out = self.disc_model(hr_img).mean()
+        fake_out = self.disc_model(sr_img).mean()
+        self.logger.debug(f'real_out = {real_out}')
+        self.logger.debug(f'fake_out = {fake_out}')
+        disc_loss = -torch.log(1-fake_out) - torch.log(real_out)
+        self.logger.debug(f'disc_loss = {disc_loss}')
+        disc_loss.backward()
+        self.disc_optimizer.step()
+
+        self.gen_model.zero_grad()
+        sr_img = self.gen_model(lr_img)
+        fake_out = self.disc_model(sr_img).mean()
+
+        gen_loss = self.gen_criterion(fake_out, sr_img, hr_img)
+        gen_loss.backward()
+        self.gen_optimizer.step()
+
+        return (
+            gen_loss.item(),
+            disc_loss.item(),
+            fake_out.item(),
+            real_out.item(),
+        )
 
     def _train_epoch(self, loader):
         if not loader:
@@ -158,11 +269,12 @@ class Trainer:
             raise ValueError(msg)
 
         self.gen_model.train()
-        self.disc_model.train()
+        if self.has_disc:
+            self.disc_model.train()
         self.gen_criterion.clear_losses()
 
         gen_epoch_loss, disc_epoch_loss = 0, 0
-        gen_score, disc_score = 0, 0
+        gen_epoch_score, disc_epoch_score = 0, 0
         i = 0
         for i, data in enumerate(loader):
             self.logger.debug(f'========= BATCH N{i} ==========')
@@ -172,49 +284,32 @@ class Trainer:
             self.logger.debug(f'hr_img info: shape = {hr_img.shape}')
             self.logger.debug(f'lr_img info: shape = {lr_img.shape}')
 
-            sr_img = self.gen_model(lr_img)
-            self.logger.debug(f'sr_img info: shape = {sr_img.shape}')
-
-            self.disc_model.zero_grad()
-
-            real_out = self.disc_model(hr_img).mean()
-            fake_out = self.disc_model(sr_img).mean()
-            self.logger.debug(f'real_out = {real_out}')
-            self.logger.debug(f'fake_out = {fake_out}')
-            disc_loss = -torch.log(1-fake_out) - torch.log(real_out)
-            self.logger.debug(f'disc_loss = {disc_loss}')
-            disc_loss.backward()
-            self.disc_optimizer.step()
-
-            self.gen_model.zero_grad()
-            sr_img = self.gen_model(lr_img)
-            fake_out = self.disc_model(sr_img).mean()
-
-            gen_loss = self.gen_criterion(fake_out, sr_img, hr_img)
-            gen_loss.backward()
-            self.gen_optimizer.step()
-
-            gen_epoch_loss += gen_loss.item()
-            disc_epoch_loss += disc_loss.item()
-            gen_score += fake_out.item()
-            disc_score += real_out.item()
+            gen_loss, disc_loss, gen_score, disc_score = self._step_batch(
+                lr_img,
+                hr_img,
+            )
+            gen_epoch_loss += gen_loss
+            disc_epoch_loss += disc_loss
+            gen_epoch_score += gen_score
+            disc_epoch_score += disc_score
             self.logger.debug(f'gen_loss = {gen_loss}')
             self.logger.debug(f'disc_loss = {disc_loss}')
             self.logger.debug(f'gen_score = {gen_score}')
             self.logger.debug(f'disc_score = {disc_score}')
         i += 1
 
-        gen_score /= i
-        disc_score /= i
+        gen_epoch_score /= i
+        disc_epoch_score /= i
         gen_epoch_loss /= i
         disc_epoch_loss /= i
-        return gen_epoch_loss, disc_epoch_loss, gen_score, disc_score
+        return (gen_epoch_loss, disc_epoch_loss,
+                gen_epoch_score, disc_epoch_score)
 
     def _eval_epoch(self, loader, epoch):
         self.gen_model.eval()
 
         with torch.no_grad():
-            evaling_results = {'mse': 0, 'psnr': 0, 'ssim': 0}
+            evaling_results = {'mse': 0.0, 'psnr': 0.0, 'ssim': 0.0}
             eval_images = []
             i = 0
             for i, (eval_lr, eval_restored, eval_hr) in enumerate(loader):
@@ -222,7 +317,6 @@ class Trainer:
                 eval_hr = eval_hr.cuda()
 
                 eval_sr = self.gen_model(eval_lr)
-
                 single_mse = torch.nn.functional.mse_loss(eval_sr, eval_hr)
                 evaling_results['mse'] += single_mse.item()
 
@@ -259,13 +353,6 @@ class Trainer:
 
         return evaling_results
 
-    def save_model(self, gen_model, gen_name, disc_model, disc_name):
-        models_path = Path('models')
-        if not models_path.exists():
-            models_path.mkdir()
-        torch.save(gen_model.state_dict(), models_path / f'{gen_name}.pt')
-        torch.save(disc_model.state_dict(), models_path / f'{disc_name}.pt')
-
     def save_metric_data(self, name, epoch):
         out_path = Path('statistics/')
         data_frame = pd.DataFrame(
@@ -275,7 +362,7 @@ class Trainer:
                   'Score_G': self.get_metric_list('gen_score'),
                   'PSNR': self.get_metric_list('psnr'),
                   'SSIM': self.get_metric_list('ssim')},
-            index=range(1, epoch + 1))
+            index=range(0, epoch))
         data_frame.to_csv(out_path / f'{name}.csv', index_label='Epoch')
 
     def get_metric_list(self, metric_name):
@@ -300,15 +387,11 @@ if __name__ == "__main__":
     disc_optimizer = torch.optim.Adam
     gen_optimizer_params = {'lr': 5e-4}
     disc_optimizer_params = {'lr': 2e-5}
-    trainer = Trainer(crop_size=150, epochs=180, gen_optimizer=gen_optimizer,
+    aug_lst = ['plain', 'extended', 'photo', 'game', 'video']
+    for aug_type in aug_lst
+    trainer = Trainer(crop_size=150, epochs=30, gen_optimizer=gen_optimizer,
                       disc_optimizer=disc_optimizer,
                       gen_optimizer_params=gen_optimizer_params,
-                      disc_optimizer_params=disc_optimizer_params)
+                      disc_optimizer_params=disc_optimizer_params,
+                      model_type='full')
     trainer.fit(train_dir, eval_dir, batch_size=24)
-    plt.plot(trainer.get_metric_list('mse'))
-    plt.plot(trainer.get_metric_list('ssim'))
-    plt.show()
-    print('===============================')
-    plt.plot(trainer.get_metric_list('gen_score'))
-    plt.plot(trainer.get_metric_list('disc_score'))
-    plt.show()
